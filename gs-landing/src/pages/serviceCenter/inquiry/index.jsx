d3169ccdc412b694e3edd1192673f6170849ae8c
bongjunk <mydlwm_s@naver.com>
2023-03-16 17:16:52 +0900

    gs-landing 삭제

--- gs-landing/src/pages/serviceCenter/inquiry/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import CustomerInquiry from "../../../components/serviceCenter/customerInquiry";
-export default function ServiceCenter() {
-  return (
-    <>
-      <CustomerInquiry />
-    </>
-  );
-}
